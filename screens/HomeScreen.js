f24b8cdcfa3bfd3089cee7639154ff11419785e0
incognitoDevOps <in110005620@kisiiuniversity.ac.ke>
2024-02-15 23:52:14 +0300

    editing home screen data

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -83,7 +83,21 @@ const dummyFeaturesData = [
     description:
       "Discover the latest tourist attraction in Masai Mara.",
       image: require("../assets/destination1.jpeg"),
-  },
+      image2: require("../assets/hotel1.jpeg"),
+      image3: require("../assets/hotel1.jpeg"),
+      description:`Welcome to the enchanting Nyamira Manga Hills, a captivating tourist attraction nestled in the heart of Nyamira County, Kenya. Immerse yourself in the breathtaking beauty of this picturesque destination, where lush greenery, rolling hills, and vibrant landscapes create a mesmerizing tapestry of nature's wonders.
+  
+        Nyamira Manga Hills offers a serene retreat for nature enthusiasts and adventure seekers alike. As you ascend the hills, you'll be treated to panoramic views that stretch as far as the eye can see. The undulating terrain, adorned with tea plantations and indigenous flora, adds to the allure of this idyllic setting.
+  
+        `,
+        description2:`
+        The air is crisp and invigorating, making it the perfect escape from the hustle and bustle of everyday life. Whether you're an avid hiker or simply seeking a peaceful getaway, Nyamira Manga Hills has something for everyone. Hike along well-maintained trails, where every step unveils new vistas and the opportunity to connect with the unspoiled beauty of the Kenyan countryside.
+        
+        The charm of Nyamira Manga Hills isn't limited to its natural splendor; the local culture adds an extra layer of richness to the experience. Engage with the warm and welcoming communities that call this region home, and discover the traditions and customs that have been passed down through generations.
+        `,
+        description3:`
+       For those looking to capture the magic of Nyamira Manga Hills, don't forget your camera. Sunrise and sunset paint the hills in hues of gold and pink, creating a captivating spectacle that's sure to leave a lasting impression.`,
+    },
   {
     id: "2",
     county: "Nairobi",
@@ -91,7 +105,21 @@ const dummyFeaturesData = [
     description:
       "Discover the latest tourist attraction in Masai Mara.",
       image: require("../assets/destination2.jpeg"),
-  },
+      image2: require("../assets/hotel1.jpeg"),
+      image3: require("../assets/hotel1.jpeg"),
+      description:`Welcome to the enchanting Nyamira Manga Hills, a captivating tourist attraction nestled in the heart of Nyamira County, Kenya. Immerse yourself in the breathtaking beauty of this picturesque destination, where lush greenery, rolling hills, and vibrant landscapes create a mesmerizing tapestry of nature's wonders.
+  
+        Nyamira Manga Hills offers a serene retreat for nature enthusiasts and adventure seekers alike. As you ascend the hills, you'll be treated to panoramic views that stretch as far as the eye can see. The undulating terrain, adorned with tea plantations and indigenous flora, adds to the allure of this idyllic setting.
+  
+        `,
+        description2:`
+        The air is crisp and invigorating, making it the perfect escape from the hustle and bustle of everyday life. Whether you're an avid hiker or simply seeking a peaceful getaway, Nyamira Manga Hills has something for everyone. Hike along well-maintained trails, where every step unveils new vistas and the opportunity to connect with the unspoiled beauty of the Kenyan countryside.
+        
+        The charm of Nyamira Manga Hills isn't limited to its natural splendor; the local culture adds an extra layer of richness to the experience. Engage with the warm and welcoming communities that call this region home, and discover the traditions and customs that have been passed down through generations.
+        `,
+        description3:`
+       For those looking to capture the magic of Nyamira Manga Hills, don't forget your camera. Sunrise and sunset paint the hills in hues of gold and pink, creating a captivating spectacle that's sure to leave a lasting impression.`,
+    },
   {
     id: "3",
     county: "Muranga",
@@ -99,7 +127,21 @@ const dummyFeaturesData = [
     description:
       "Discover the latest tourist attraction in Masai Mara.",
       image: require("../assets/destination3.jpeg"),
-  },
+      image2: require("../assets/hotel1.jpeg"),
+      image3: require("../assets/hotel1.jpeg"),
+      description:`Welcome to the enchanting Nyamira Manga Hills, a captivating tourist attraction nestled in the heart of Nyamira County, Kenya. Immerse yourself in the breathtaking beauty of this picturesque destination, where lush greenery, rolling hills, and vibrant landscapes create a mesmerizing tapestry of nature's wonders.
+  
+        Nyamira Manga Hills offers a serene retreat for nature enthusiasts and adventure seekers alike. As you ascend the hills, you'll be treated to panoramic views that stretch as far as the eye can see. The undulating terrain, adorned with tea plantations and indigenous flora, adds to the allure of this idyllic setting.
+  
+        `,
+        description2:`
+        The air is crisp and invigorating, making it the perfect escape from the hustle and bustle of everyday life. Whether you're an avid hiker or simply seeking a peaceful getaway, Nyamira Manga Hills has something for everyone. Hike along well-maintained trails, where every step unveils new vistas and the opportunity to connect with the unspoiled beauty of the Kenyan countryside.
+        
+        The charm of Nyamira Manga Hills isn't limited to its natural splendor; the local culture adds an extra layer of richness to the experience. Engage with the warm and welcoming communities that call this region home, and discover the traditions and customs that have been passed down through generations.
+        `,
+        description3:`
+       For those looking to capture the magic of Nyamira Manga Hills, don't forget your camera. Sunrise and sunset paint the hills in hues of gold and pink, creating a captivating spectacle that's sure to leave a lasting impression.`,
+    },
   {
     id: "4",
     county: "Mombasa",
@@ -107,7 +149,21 @@ const dummyFeaturesData = [
     description:
       "Discover the latest tourist attraction in Masai Mara.",
       image: require("../assets/destination4.jpeg"),
-  },
+      image2: require("../assets/hotel1.jpeg"),
+      image3: require("../assets/hotel1.jpeg"),
+      description:`Welcome to the enchanting Nyamira Manga Hills, a captivating tourist attraction nestled in the heart of Nyamira County, Kenya. Immerse yourself in the breathtaking beauty of this picturesque destination, where lush greenery, rolling hills, and vibrant landscapes create a mesmerizing tapestry of nature's wonders.
+  
+        Nyamira Manga Hills offers a serene retreat for nature enthusiasts and adventure seekers alike. As you ascend the hills, you'll be treated to panoramic views that stretch as far as the eye can see. The undulating terrain, adorned with tea plantations and indigenous flora, adds to the allure of this idyllic setting.
+  
+        `,
+        description2:`
+        The air is crisp and invigorating, making it the perfect escape from the hustle and bustle of everyday life. Whether you're an avid hiker or simply seeking a peaceful getaway, Nyamira Manga Hills has something for everyone. Hike along well-maintained trails, where every step unveils new vistas and the opportunity to connect with the unspoiled beauty of the Kenyan countryside.
+        
+        The charm of Nyamira Manga Hills isn't limited to its natural splendor; the local culture adds an extra layer of richness to the experience. Engage with the warm and welcoming communities that call this region home, and discover the traditions and customs that have been passed down through generations.
+        `,
+        description3:`
+       For those looking to capture the magic of Nyamira Manga Hills, don't forget your camera. Sunrise and sunset paint the hills in hues of gold and pink, creating a captivating spectacle that's sure to leave a lasting impression.`,
+    },
   {
     id: "5",
     county: "Narok",
@@ -115,7 +171,21 @@ const dummyFeaturesData = [
     description:
       "Discover the latest tourist attraction in Masai Mara.",
       image: require("../assets/destination5.jpeg"),
-  },
+      image2: require("../assets/hotel1.jpeg"),
+      image3: require("../assets/hotel1.jpeg"),
+      description:`Welcome to the enchanting Nyamira Manga Hills, a captivating tourist attraction nestled in the heart of Nyamira County, Kenya. Immerse yourself in the breathtaking beauty of this picturesque destination, where lush greenery, rolling hills, and vibrant landscapes create a mesmerizing tapestry of nature's wonders.
+  
+        Nyamira Manga Hills offers a serene retreat for nature enthusiasts and adventure seekers alike. As you ascend the hills, you'll be treated to panoramic views that stretch as far as the eye can see. The undulating terrain, adorned with tea plantations and indigenous flora, adds to the allure of this idyllic setting.
+  
+        `,
+        description2:`
+        The air is crisp and invigorating, making it the perfect escape from the hustle and bustle of everyday life. Whether you're an avid hiker or simply seeking a peaceful getaway, Nyamira Manga Hills has something for everyone. Hike along well-maintained trails, where every step unveils new vistas and the opportunity to connect with the unspoiled beauty of the Kenyan countryside.
+        
+        The charm of Nyamira Manga Hills isn't limited to its natural splendor; the local culture adds an extra layer of richness to the experience. Engage with the warm and welcoming communities that call this region home, and discover the traditions and customs that have been passed down through generations.
+        `,
+        description3:`
+       For those looking to capture the magic of Nyamira Manga Hills, don't forget your camera. Sunrise and sunset paint the hills in hues of gold and pink, creating a captivating spectacle that's sure to leave a lasting impression.`,
+    },
 ];
 
 const HomeScreen = ({ navigation }) => {
