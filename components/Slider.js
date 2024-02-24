8988a5475b422c4c4812abf71770ecfbd0be4460
incognitoDevOps <in110005620@kisiiuniversity.ac.ke>
2024-02-24 19:59:26 +0300

    slides

--- components/Slider.js
+++ components/Slider.js
@@ -9,30 +9,33 @@ import {
   Image,
 } from "react-native";
 import Swiper from "react-native-swiper";
-import image1 from '../assets/hotel1.jpeg';
+import image1 from "../assets/hotel1.jpeg";
 import image2 from "../assets/kit-mikayi-poster.jpg";
 import image3 from "../assets/hotel2.jpeg";
 
-
 export default function Slider() {
-
-//   const images = [image1, image2,image3];
+  //   const images = [image1, image2,image3];
   const images = [
-    { image: image1, text: 'Welcome to your app' },
-    { image: image2, text: 'Welcome to your app' },
-    { image: image3, text: 'Welcome to your app' },
+    { image: image1, text: "Welcome to your app" },
+    { image: image2, text: "Welcome to your app" },
+    { image: image3, text: "Welcome to your app" },
   ];
-  const timer = 5000
+  const timer = 5000;
   return (
-    <Swiper style={styles.wrapper} showsButtons={false} autoplay={true}
-    autoplayTimeout={10} key={`swiper-${Math.random()}`}>
+    <Swiper
+      style={styles.wrapper}
+      showsButtons={false}
+      autoplay={true}
+      autoplayTimeout={10}
+      key={`swiper-${Math.random()}`}
+    >
       {images.map((item, index) => (
         <View key={index} style={styles.slide}>
           <Image source={item.image} style={styles.image} />
           <View style={styles.overlay}>
-              <Text style={styles.text}>{item.text}</Text>
-              <TextInput style={styles.TextInput} placeholder="Search"/>
-            </View>
+            <Text style={styles.text}>{item.text}</Text>
+            <TextInput style={styles.TextInput} placeholder="Search" />
+          </View>
         </View>
       ))}
     </Swiper>
@@ -40,43 +43,40 @@ export default function Slider() {
 }
 
 const styles = StyleSheet.create({
-    wrapper: {
-        height: 200,
-      },
-      slideContainer: {
-        flex: 1,
-      },
-      slide: {
-        flex: 1,
-        position: 'relatfive',
-      },
-      image: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'cover',
-      },
-      overlay: {
-        ...StyleSheet.absoluteFillObject,
-        backgroundColor: 'rgba(0, 0, 0, 0.5)', 
-        
-      },
-      text: {
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 'bold',
-        paddingLeft:25,
-        paddingTop:50,
-      },
-      TextInput:{
-        borderColor: 'white',
-        borderWidth: 1,
-        borderRadius: 8,
-        margin:25,
-        width:'60%',
-        color:'white',
-        padding:5,
-        fontSize:14,
-      },
-    
-
+  wrapper: {
+    height: 200,
+  },
+  slideContainer: {
+    flex: 1,
+  },
+  slide: {
+    flex: 1,
+    position: "relatfive",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingLeft: 25,
+    paddingTop: 50,
+  },
+  TextInput: {
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 8,
+    margin: 25,
+    width: "60%",
+    color: "white",
+    padding: 5,
+    fontSize: 14,
+  },
 });
